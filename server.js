90ae6c842b1f4661413825a08918aad28a27ea11
Sreeram <sreeram.r@hapitoe.com>
2017-02-04 19:52:03 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express=require('express');
+var app=express();
+var port = 3000;
+
+app.use('/', express.static(__dirname+'/public'));
+app.listen(port,function(){
+  console.log("server running on port"+port);
+})
